35eb362db0cd0656505520e7646ce850c6fbc323
Alicya lee <sunghee2820@gmail.com>
2023-03-22 21:21:51 +0900

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://ai-chat-nnrg.onrender.com', {
+    const response = await fetch('https://al-chat2.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
